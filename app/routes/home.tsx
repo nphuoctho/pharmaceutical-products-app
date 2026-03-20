69fbc5ece2cb7eb953c847ca903bf2f5f0f0f664
nphuoctho <nguyenphuoctho0310@gmail.com>
2026-03-20 07:12:47 +0700

    feat: initial commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,13 @@
+export function meta() {
+	return [
+		{ title: 'Pharmaceutical Products' },
+		{
+			name: 'description',
+			content: 'Pharmaceutical Products app search engine'
+		}
+	];
+}
+
+export default function Home() {
+	return <h1>Hello</h1>;
+}
